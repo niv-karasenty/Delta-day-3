58264d69704bb93c88db87b7e0bb184c960d55c5
niv karasenty <niv.karas3nty@gmail.com>
2025-11-18 15:20:12 +0200

    redo exercise print value addr

--- print_value_addr.c
+++ print_value_addr.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define ARRAY_SIZE 8
+#define ARRAY_SIZE (8)
 
 void print_int_by_array(int array[]) {
     printf("Printing int array by array: \n");
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d, ", array[i]); // Printing by index in the array
+        printf("The address: %p, value: %d \n", &array[i], array[i]); // Printing by index in the array
     }
     printf("\n");
 }
@@ -13,15 +13,7 @@ void print_int_by_array(int array[]) {
 void print_int_by_ptr(int *ptr) {
     printf("Printing int array by pointer: \n");
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d, ", *(ptr + i)); // Printing by the pointer plus offset
-    }
-    printf("\n");
-}
-
-void print_int_array_address(int *ptr) {
-    printf("Printing addresses of array: \n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%p, ", (ptr + i)); // Printing addresses by not using *
+        printf("The address: %p, value: %d \n", (ptr+i), *(ptr + i)); // Printing by the pointer plus offset
     }
     printf("\n");
 }
@@ -29,7 +21,7 @@ void print_int_array_address(int *ptr) {
 void print_char_by_array(char array[]) {
     printf("Printing char array by array: \n");
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%c, ", array[i]); // printing by index in the array
+        printf("The address: %p, value: %c \n", &array[i], array[i]); // printing by index in the array
     }
     printf("\n");
 }
@@ -37,17 +29,16 @@ void print_char_by_array(char array[]) {
 void print_char_array_by_ptr(char *ptr) {
     printf("Printing char array by pointer: \n");
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%c, ", *(ptr + i)); // Printing by the offset i
+        printf("The address: %p, value: %c \n", (ptr+i), *(ptr + i)); // Printing by the offset i
     }
     printf("\n");
 }
 
-void print_char_array_address(char *ptr) {
-    printf("Printing char array address: \n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%p, ", ptr + i); // Printing addresses by not using * and still using the offset i
-    }
-    printf("\n");
+void print_all(int *int_ptr, char *char_ptr, int num_array[], char char_array[]) {
+    print_int_by_array(num_array);
+    print_char_by_array(char_array);
+    print_int_by_ptr(int_ptr);
+    print_char_array_by_ptr(char_ptr);
 }
 
 int main() {
@@ -59,23 +50,23 @@ int main() {
     // Get the inputs for the arrays
     for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("Enter %d number: ", i+1);
-        scanf("%d", &number_array[i]);
+        if (scanf("%d", &number_array[i]) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
     }
-
     for (int i = 0; i < ARRAY_SIZE; i++) {
         printf("Enter %d char: \n", i+1);
-        scanf("\n%c", &char_array[i]);
+        if (scanf("\n%c", &char_array[i]) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
     }
+
+    print_all(number_ptr, char_ptr, number_array, char_array);
 
     printf("\n");
 
-    // Going through the functions of printing
-    print_int_by_array(number_ptr);
-    print_char_by_array(char_array);
-    print_int_by_ptr(number_ptr);
-    print_char_array_by_ptr(char_ptr);
-    print_int_array_address(number_ptr);
-    print_char_array_address(char_ptr);
 
     return 0;
 }
